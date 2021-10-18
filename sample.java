b0c818ddf549351acb5b1e6141fa80beb51abd01
to_matih <maatteeoh@wp.pl>
2021-10-18 19:43:41 +0100

    fixed indentations

--- sample.java
+++ sample.java
@@ -1,8 +1,8 @@
 public class sample{
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
         System.out.println("Line printed by Aysha");
 
-		System.out.println("Line added by Mateusz")
-	}
+        System.out.println("Line added by Mateusz")
+    }
 }
